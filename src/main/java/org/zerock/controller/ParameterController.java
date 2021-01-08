cbf7cd5ca71cec3a77b2d77a45cd4e908a16d7ab
sebaek <sebaek@gmail.com>
2021-01-08 14:29:30 +0900

    param

--- src/main/java/org/zerock/controller/ParameterController.java
+++ src/main/java/org/zerock/controller/ParameterController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,7 +19,31 @@ public class ParameterController {
 		log.info(request.getParameter("name"));
 		log.info("method1");
 	}
+	
+	
+	@RequestMapping("/ex2")
+	public void method2(@RequestParam("name") String n) {
+		log.info("method2");
+		log.info(n);
+	}
+	
+	@RequestMapping("/ex3")
+	public void method3(@RequestParam String name) {
+		log.info("method3");
+		log.info(name);
+	}
+	
+	@RequestMapping("/ex4")
+	public void method4(String name) {
+		log.info("method4");
+		log.info(name);
+	}
+	
+	
 }
+
+
+
 
 
 
