b5b88e189789673832a398c711a64d8d1b60ce28
sebaek <sebaek@gmail.com>
2021-01-11 12:25:39 +0900

    model

--- src/main/java/org/zerock/controller/ModelController.java
+++ src/main/java/org/zerock/controller/ModelController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.Member;
 
 import lombok.extern.log4j.Log4j;
 
@@ -26,8 +27,40 @@ public class ModelController {
 		log.info("method2");
 		
 //		model.addAttribute("name", name);
+	}
+	
+	@RequestMapping("/ex3")
+	public void method3(@ModelAttribute("name")String name,
+			@ModelAttribute("age") int age) {
+		log.info("method3");
+		log.info(name);
+		log.info(age);
+	}
+	
+	@RequestMapping("/ex4")
+	public void method4(@ModelAttribute("member") Member member) {
+		log.info("method4");
+		log.info(member);
 		
+//		model.addAttribute("member", member);
 		
+	}
+	
+	@RequestMapping("/ex5")
+	// model attribute의 이름은 소문자로 바꾼 type 명으로 결정됨
+	public void method5(@ModelAttribute Member member) {
+		log.info("method5");
+		log.info(member);
+		
+	}
+	
+	@RequestMapping("/ex6")
+	// model attribute의 이름은 소문자로 바꾼 type 명으로 결정됨
+	public String method6(Member member) {
+		log.info("method6");
+		log.info(member);
+		
+		return "model/ex4";
 	}
 }
 
