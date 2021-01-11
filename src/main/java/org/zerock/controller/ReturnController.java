d4dfa71f0f1b232886cfcb8c2f550e03eaf88e17
sebaek <sebaek@gmail.com>
2021-01-11 11:20:35 +0900

    void

--- src/main/java/org/zerock/controller/ReturnController.java
+++ src/main/java/org/zerock/controller/ReturnController.java
@@ -2,6 +2,7 @@ package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.log4j.Log4j;
 
@@ -25,6 +26,19 @@ public class ReturnController {
 		// 일을 마치면
 		// forward or redirect
 		return "redirect:/sample/";
+	}
+	
+	@RequestMapping("/ex3")
+	public @ResponseBody String method3() {
+		log.info("method3");
+		
+		return "returnValue3 hello world~!~!~!~";
+	}
+	
+	
+	@RequestMapping("/ex4")
+	public void method4() {
+		log.info("method4");
 	}
 }
 
