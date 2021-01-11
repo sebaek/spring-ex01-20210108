736d0cd56986d52bac82a02fdea62e2bd4df8690
sebaek <sebaek@gmail.com>
2021-01-11 09:42:09 +0900

    customeditor

--- src/main/java/org/zerock/controller/ParameterController.java
+++ src/main/java/org/zerock/controller/ParameterController.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.Book;
+import org.zerock.domain.CustomBookEditor;
 import org.zerock.domain.Member;
 
 import lombok.extern.log4j.Log4j;
@@ -91,6 +95,25 @@ public class ParameterController {
 	public void method9(Member member) {
 		log.info("method9");
 		log.info(member);
+	}
+	
+
+	
+	
+	@InitBinder
+	public void initBinder1(WebDataBinder binder) {
+		log.info("initbinder1");
+		
+		// requiredType은 propertyEditor를 사용
+//		binder.registerCustomEditor(requiredType, propertyEditor);
+		binder.registerCustomEditor(Book.class, new CustomBookEditor());
+		
+	}
+	
+	@RequestMapping("/ex10")
+	public void method10(@RequestParam("book") Book book) {
+		log.info("method10");
+		log.info(book);
 	}
 }
 
