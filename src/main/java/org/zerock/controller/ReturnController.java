dff7000ecd3aabb56839934c30dcf95d8a39a8d7
sebaek <sebaek@gmail.com>
2021-01-11 10:35:32 +0900

    jsp

---
+++ src/main/java/org/zerock/controller/ReturnController.java
@@ -0,0 +1,23 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/return/*")
+@Log4j
+public class ReturnController {
+
+	
+	@RequestMapping("/ex1")
+	public String method1() {
+		log.info("method1");
+		return "returnView1";
+	}
+}
+
+
+
+
