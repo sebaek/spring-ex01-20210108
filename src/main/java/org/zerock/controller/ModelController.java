e9ee06a631b0dc05f78fb9bccfdd006cfbfca31e
sebaek <sebaek@gmail.com>
2021-01-11 12:08:34 +0900

    model

--- src/main/java/org/zerock/controller/ModelController.java
+++ src/main/java/org/zerock/controller/ModelController.java
@@ -2,6 +2,7 @@ package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
@@ -19,7 +20,20 @@ public class ModelController {
 		model.addAttribute("name", "java");
 		
 	}
+	
+	@RequestMapping("/ex2")
+	public void method2(@ModelAttribute("name") String name) {
+		log.info("method2");
+		
+//		model.addAttribute("name", name);
+		
+		
+	}
 }
+
+
+
+
 
 
 
